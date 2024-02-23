d2a797d1540e39933abe6486424a339d4b3e48cd
timtam8181 <14898@coderacademy.edu.au>
2024-02-23 21:49:48 +1100

    Added Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from 'react';
+import NavBar from './NavBar';
+import './Details.css';
 
 const Details = () => {
   return (
-    
     <>
-    <NavBar />
-    <body>
-    </body>
-  
-  </>
-  )
-}
+      <NavBar />
+      <div className="background-image"></div> 
+      <div className="details-container">
+        <div className="left-bubble">
+          <h2 className="details-heading">When and Where</h2>
+          <div className="bubble">
+            <p>Date: 08/09/2024</p>
+            <p>Time: 4pm</p>
+            <p>Location: Bramleigh Estate</p>
+          </div>
+        </div>
+        <div className="right-bubble">
+          <h2 className="details-heading">Itinerary</h2>
+          <div className="bubble">
+            <ul>
+              <li>Arrival & Greetings</li>
+              <li>Ceremony</li>
+              <li>Reception</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Details
+export default Details;
