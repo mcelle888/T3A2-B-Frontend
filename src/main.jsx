34836af038e14ea9d988b9126a3ec8d74b68d5cd
mcelle888 <14894@coderacademy.edu.au>
2024-02-27 21:32:45 +1100

    Fixed folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './index.css'
+import App from './App.jsx'
+import './css/index.css'
 
 // Renders root component into DOM (renders the App comonent inside the root element of HTML document)
 ReactDOM.createRoot(document.getElementById('root')).render(
